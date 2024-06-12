99b86753ea4f2b160e7895d9b17caf4f771764c6
Vincent <sinceritymaiden@hotmail.com>
2024-06-12 16:12:39 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import ReactJson from 'react-json-view'
+import ReactJson from "react-json-view";
 
 // import the fingerprintjs opensource library
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
@@ -15,6 +15,20 @@ export default function App() {
     const setFp = async () => {
       const fp = await FingerprintJS.load();
       const fpResult = await fp.get();
+      fpResult.meta = {};
+      // console.log(navigator);
+
+      const entrophy = await navigator.userAgentData.getHighEntropyValues([
+        "architecture",
+        "model",
+        "platformVersion",
+        "fullVersionList",
+      ]);
+      fpResult.meta.userAgent = navigator.userAgent;
+      fpResult.meta.architecture = entrophy.architecture;
+      fpResult.meta.isMobile = entrophy.mobile;
+      fpResult.meta.platform = entrophy.platform;
+      fpResult.meta.platformVersion = entrophy.platformVersion;
 
       setFpHash(fpResult.visitorId);
       setFpInfo(fpResult);
@@ -26,7 +40,7 @@ export default function App() {
     <div>
       <h1>This is the fingerprint hash</h1>
       <h3>Hash: {fpHash}</h3>
-      <hr/>
+      <hr />
       <ReactJson src={fpInfo} />
     </div>
   );
